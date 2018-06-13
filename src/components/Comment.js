0225522d3fa72d9691095da93477686c6dba8a1d
bgirlcoolcat <evansb21@gmail.com>
2018-06-13 16:50:04 +0100

    Broke comment out into a component; amends to delete a comment

---
+++ src/components/Comment.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import './Comments.css';
+
+class Comment extends Component {
+  state = {};
+
+  handleDeleteCommentClick = () => {
+    // Remember it is what we have called the prop in the parent, e.g. id, not _id
+    this.props.onDeleteComment(this.props.id);
+  }
+
+  render () {
+      const date = moment(this.props.created_at)
+      return (
+        <article className="comments">
+
+            <div className="comment-vote-count">
+              <button className="btn-vote">
+                <span className="far fa-thumbs-up" span-toggle="fas fa-thumbs-up" data-toggle="tooltip" title="Vote up"></span>
+              </button>
+                <p>{this.props.votes || 0}</p>
+              <button className="btn-vote">
+                <span className="far fa-thumbs-down" data-toggle="tooltip" title="Vote down"></span>
+              </button>
+            </div>
+            
+            <div className="comment-article">
+              <p className="commentator">Posted by <Link to={`/users/${this.props.created_by}`}>{this.props.created_by || "northcoder"}</Link><span> - </span>{date.startOf('minute').fromNow()}</p>
+              <p className="comment-text">{this.props.body || this.props.comment}</p>
+              
+              {this.props.created_by === "northcoder" ? 
+              <button className="btn-delete-comment" onClick={this.handleDeleteCommentClick}>
+              Delete comment
+              </button> : 
+              <button className="btn-delete-comment" onClick={()=>{ alert('You are not authorised to delete a comment posted by another user') }}>
+              Delete comment
+              </button>}
+            </div>
+            
+        </article>
+    );
+  }
+}
+
+export default Comment;
--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { fetchComments, deleteComment } from '../containers/api';
 import Loading from './Loading';
-import moment from 'moment';
 import AddCommentsForm from './AddCommentForm';
-import { Link } from 'react-router-dom';
 // import uuid from 'uuid';
+import Comment from './Comment';
 import './Comments.css';
 
 class Comments extends Component {
@@ -40,25 +39,20 @@ class Comments extends Component {
 
     const newComments = this.state.comments.concat([newComment]);
 
-    this.setState({ 
-      comments: newComments 
-    });
+    this.setState({ comments: newComments });
   };
 
-  removeNCComment = comment => {
-    // console.log('Remove NCComment:', comment);
+  handleDeleteCommentEvent = commentId => {
+    // console.log('Remove NCComment:', commentId);
 
-    if (comment.created_by === "northcoder") {
       const updatedComments = this.state.comments.filter(item => {
-        return item !== comment;
+        return item._id !== commentId;
       })
 
-      this.setState({
-        comments: updatedComments
-      })
+      this.setState({comments: updatedComments})
 
-      deleteComment(comment._id)
-    }
+      deleteComment(commentId)
+
   };
 
   render () {
@@ -67,31 +61,27 @@ class Comments extends Component {
       <div>
         <div className="comments">
           <h4>Comments</h4>
-          {/* <a href="">Add your own comment...</a> */}
           <AddCommentsForm addComment={this.addComment} articleId={articleId} />
         </div>  
-        <div>
+
           { loading ? <Loading /> : 
             comments.map((comment, commentId) => {
-              const date = moment(comment.created_at);
               return (
-                <article key={comment._id || commentId} className="container">
-                  <div className="comment-vote-count">
-                    <span className="far fa-thumbs-up"></span>
-                    <p>{comment.votes || 0}</p>
-                    <span className="far fa-thumbs-down"></span>
-                  </div>
-                  <div className="comment-article">
-                    <p className="commentator">Posted by <Link to={`/users/${comment.created_by}`}>{comment.created_by || "northcoder"}</Link><span> - </span>{date.startOf('minute').fromNow()}</p>
-                    <p className="comment-text">{comment.body || comment.comment}</p>
-                    {comment.created_by === "northcoder" ? <button className="btn-delete-comment" onClick={(e) => this.removeNCComment(comment)}>Delete comment</button> : <button className="btn-delete-comment" onClick={()=>{ alert('You are not authorised to delete a comment posted by another user') }}>Delete comment</button>}
-                  </div>
-                </article>
-              );
+                // <article key={comment._id || commentId} className="container">
+                  <Comment 
+                  key={comment._id || commentId}
+                  id={comment._id || commentId}
+                  created_at={comment.created_at}
+                  created_by={comment.created_by}
+                  votes={comment.votes}
+                  body={comment.body || comment.comment}
+                  onDeleteComment={this.handleDeleteCommentEvent}
+                />
+                // </article>
+              )
             })
           }
         </div>
-      </div>
     );
   }
 }
