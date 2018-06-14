a4510517f02e159cc4ad3c6e12840ea6163f0a6e
bgirlcoolcat <evansb21@gmail.com>
2018-06-14 13:17:23 +0100

    Increment or decrement the votes of an comment by one

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -12,11 +12,17 @@ class Comment extends Component {
   }
 
   handleUpVoteClick = () => {
-    this.props.onUpVote(this.props.id);
+    this.setState({
+      upVoted: !this.props.upVoted
+    })
     // id is what I called it in Comments.js
+    this.props.onUpVote(this.props.id);
   }
 
   handleDownVoteClick = () => {
+    this.setState({
+      downVoted: !this.props.downVoted
+    })
     this.props.onDownVote(this.props.id);
   }
 
@@ -25,28 +31,38 @@ class Comment extends Component {
       return (
         <article className="comments">
 
-            <div className="comment-vote-count">
-              <button className="btn-vote" onClick={this.handleUpVoteClick}>
-                <span className="far fa-thumbs-up" span-toggle="fas fa-thumbs-up" data-toggle="tooltip" title="Vote up"></span>
-              </button>
-                <p>{this.props.votes || 0}</p>
-              <button className="btn-vote" onClick={this.handleDownVoteClick}>
-                <span className="far fa-thumbs-down" data-toggle="tooltip" title="Vote down"></span>
-              </button>
-            </div>
+          <div className="comment-vote-count">
+          { this.state.upVoted ? 
+            <button className="btn-voted" onClick={this.handleUpVoteClick} disabled> 
+              <span className="far fa-thumbs-up" span-toggle="fas fa-thumbs-up" data-toggle="tooltip" title="You have already voted"></span>
+            </button> :
+            <button className="btn-vote" onClick={this.handleUpVoteClick}>
+              <span className="far fa-thumbs-up" span-toggle="fas fa-thumbs-up" data-toggle="tooltip" title="Vote up"></span>
+            </button> 
+          }
+            <p>{this.props.votes || 0}</p>
+          { this.state.downVoted ? 
+            <button className="btn-voted" onClick={this.handleDownVoteClick} disabled>
+              <span className="far fa-thumbs-down" data-toggle="tooltip" title="You have already voted"></span>
+            </button> :
+            <button className="btn-vote" onClick={this.handleDownVoteClick}>
+              <span className="far fa-thumbs-down" data-toggle="tooltip" title="Vote down"></span>
+            </button>
+          }
+          </div>
             
-            <div className="comment-article">
-              <p className="commentator">Posted by <Link to={`/users/${this.props.created_by}`}>{this.props.created_by || "northcoder"}</Link><span> - </span>{date.startOf('minute').fromNow()}</p>
-              <p className="comment-text">{this.props.body || this.props.comment}</p>
-              
-              {this.props.created_by === "northcoder" ? 
-              <button className="btn-delete-comment" onClick={this.handleDeleteCommentClick}>
-              Delete comment
-              </button> : 
-              <button className="btn-delete-comment" onClick={()=>{ alert('You are not authorised to delete a comment posted by another user') }}>
-              Delete comment
-              </button>}
-            </div>
+          <div className="comment-article">
+            <p className="commentator">Posted by <Link to={`/users/${this.props.created_by}`}>{this.props.created_by || "northcoder"}</Link><span> - </span>{date.startOf('minute').fromNow()}</p>
+            <p className="comment-text">{this.props.body || this.props.comment}</p>
+            
+            {this.props.created_by === "northcoder" ? 
+            <button className="btn-delete-comment" onClick={this.handleDeleteCommentClick}>
+            Delete comment
+            </button> : 
+            <button className="btn-delete-comment" onClick={()=>{ alert('You are not authorised to delete a comment posted by another user') }}>
+            Delete comment
+            </button>}
+          </div>
             
         </article>
     );
